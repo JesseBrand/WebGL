a2bb2aaf93b4370fe9173a9adba8da9fd375e3e7
Jesse Brand <jessebrand@gmail.com>
2016-06-24 13:56:08 +0200

    Implement 3D rendering of webpage.

--- assignments/jesse-sim/controls.js
+++ assignments/jesse-sim/controls.js
@@ -1,21 +1,20 @@
-/**
- * @author mrdoob / http://mrdoob.com/
- */
+THREE.MyControls = function(camera) {
 
-THREE.MyControls = function ( camera ) {
+	var self = this;
+	this.enabled = false;
+	this.cursorFree = false;
 
-	var scope = this;
-
-	camera.rotation.set( 0, 0, 0 );
+	camera.rotation.set(0, 0, 0);
 
 	var yawObject = camera;
 	yawObject.rotation.order = 'YXZ';
 	
 	var moveFactor = .002;
 
-	var onMouseMove = function ( event ) {
-
-		if ( scope.enabled === false ) return;
+	var onMouseMove = function(event) {
+		if (self.enabled === false || self.cursorFree === true) {
+			return;
+		}
 
 		var dX = (event.movementX || event.mozMovementX || event.webkitMovementX || 0) * moveFactor;
 		var dY = (event.movementY || event.mozMovementY || event.webkitMovementY || 0) * moveFactor;
@@ -33,31 +32,28 @@ THREE.MyControls = function ( camera ) {
 
 	document.addEventListener( 'mousemove', onMouseMove, false );
 
-	this.enabled = false;
-
 	this.getObject = function () {
-
 		return yawObject;
-
 	};
 
 	this.getDirection = function() {
-
-		// assumes the camera itself is not rotated
 
 		var direction = new THREE.Vector3( 0, 0, - 1 );
 		var rotation = new THREE.Euler( 0, 0, 0, "YXZ" );
 
 		return function( v ) {
-
 			rotation.set( yawObject.rotation.x, yawObject.rotation.y, 0 );
-
 			v.copy( direction ).applyEuler( rotation );
-
 			return v;
-
 		};
-
 	}();
-
+	
+	this.switchCursorMode = function() {
+		self.cursorFree = !self.cursorFree;
+		if (self.cursorFree) {
+			releaseFocus();
+		} else {
+			grabFocus();
+		}
+	}
 };
